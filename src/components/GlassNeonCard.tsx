7eb75c015dde24a443df694fb8c360971964886a
Pusaka Manggala <pusakamanggala@gmail.com>
2024-03-25 19:38:50 +0700

    style : hover styling

--- src/components/GlassNeonCard.tsx
+++ src/components/GlassNeonCard.tsx
@@ -8,15 +8,13 @@ export function GlassNeonCard({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col items-center justify-center space-y-12 neon__card min-h-[500px] max-h-[500px] group hover:justify-center py-10 md:py-20 px-5 md:px-10 ">
-      <div className="min-h-20 aspect-square group-hover:hidden">
-        {children}
-      </div>
-      <hgroup className="space-y-5">
+    <div className="flex flex-col items-center justify-center space-y-12 neon__card min-h-[500px] max-h-[500px] group py-10 md:py-20 px-5 md:px-10 relative">
+      <div className="group-hover:hidden mx-0">{children}</div>
+      <hgroup className="space-y-5 group-hover:absolute group-hover:translate-y-[-10%] transition-all duration-300 ease-in-out">
         <h3 className="font-semibold group-hover:hidden neon__title">
           {title}
         </h3>
-        <p className="flex-1 line-clamp-6 group-hover:line-clamp-none">
+        <p className="flex-1 line-clamp-6 group-hover:line-clamp-none px-5">
           {description}
         </p>
       </hgroup>
